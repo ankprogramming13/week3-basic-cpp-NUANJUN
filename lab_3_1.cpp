d8ac2d9105b4abae8c6736d8332f5e75d485de2f
Unknown <trtrinw@gmail.com>
2017-11-21 17:34:32 +0700

    ข้อที่ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Miss." << endl << "Nuanjua" << endl << "Buasir" << endl << "Nuan" << endl << "Female" << endl
+  << "27 July 1998" << endl << "watch a movie" << endl << "single" ;
+
+
+
+
   return 0;
 }
